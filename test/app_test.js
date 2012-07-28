cf4d4edbd92ac67663f03de7d23d91ea01f08f4e
Thiago Nunes <thiagotnunes@gmail.com>
2012-07-28 17:25:20 -0300

    Refactoring app test.

--- test/app_test.js
+++ test/app_test.js
@@ -11,27 +11,31 @@ describe('App', function() {
     });
   });
 
-  it('should upload a file to the server and receive a file path on the response', function(done) {
-    var image = 'test/image.png';
-    var uploadUrl = baseUrl + '/upload';
-    var uploadData = {
-      method: 'POST',
-      uri: uploadUrl,
-      multipart: [
-        {
-          'content-type': 'application/json',
-          body: JSON.stringify({yoda_attachment: 'this is'})
-        }
-      ]
-    };
-    http(uploadData, function(error, response, body) {
-      response.statusCode.should.equal(200);
-      response.headers['content-type'].should.equal('application/json; charset=utf-8');
-      var uploaded = JSON.parse(body);
-      http.get(baseUrl + uploaded.path, function(err, res, bod) {
-        res.statusCode.should.equal(200);
-        done();
+  describe('File upload', function() {
+
+    it('should upload a file to the server and receive a file path on the response', function(done) {
+      var image = 'test/image.png';
+      var uploadUrl = baseUrl + '/upload';
+      var uploadData = {
+        method: 'POST',
+        uri: uploadUrl,
+        multipart: [
+          {
+            'content-type': 'application/json',
+            body: JSON.stringify({yoda_attachment: 'this is'})
+          }
+        ]
+      };
+      http(uploadData, function(error, response, body) {
+        response.statusCode.should.equal(200);
+        response.headers['content-type'].should.equal('application/json; charset=utf-8');
+        var uploaded = JSON.parse(body);
+        http.get(baseUrl + uploaded.path, function(err, res, bod) {
+          res.statusCode.should.equal(200);
+          done();
+        });
       });
     });
   });
+
 });
