4045de9b0052db6a715b4ba2ac264fd842b54157
Thiago Nunes <thiagotnunes@gmail.com>
2012-07-28 16:37:16 -0300

    Uploading a file to the server.

--- test/app_test.js
+++ test/app_test.js
@@ -1,5 +1,6 @@
 var http = require('request');
 var should = require('should');
+var fs = require('fs');
 
 var baseUrl = 'http://localhost:3000';
 
@@ -12,10 +13,12 @@ describe('App', function() {
   });
 
   it('should upload a file to the server and receive a file path on the response', function(done) {
-    http.post(baseUrl + '/upload', function(error, response, body) {
+    var image = 'test/image.png';
+    var uploadUrl = baseUrl + '/upload';
+    fs.createReadStream(image).pipe(http.post(uploadUrl, function(error, response, body) {
       response.statusCode.should.equal(200);
       response.headers['content-type'].should.equal('application/json; charset=utf-8');
       done();
-    });
+    }));
   });
 });
