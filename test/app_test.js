2f9c439f9af5abb184deb057dba084cb2e5c1fb9
Thiago Nunes <thiagotnunes@gmail.com>
2012-07-28 17:36:36 -0300

    Refactoring app test.

--- test/app_test.js
+++ test/app_test.js
@@ -13,25 +13,28 @@ describe('App', function() {
 
   describe('File upload', function() {
 
+    var uploadUrl = baseUrl + '/upload';
+    var attachment = {yoda: 'i am'};
+    var uploadData = {
+      method: 'POST',
+      uri: uploadUrl,
+      multipart: [
+        {
+          'content-type': 'application/json',
+          body: JSON.stringify(attachment)
+        }
+      ]
+    };
+
     it('should upload a file to the server and receive a file path on the response', function(done) {
-      var image = 'test/image.png';
-      var uploadUrl = baseUrl + '/upload';
-      var uploadData = {
-        method: 'POST',
-        uri: uploadUrl,
-        multipart: [
-          {
-            'content-type': 'application/json',
-            body: JSON.stringify({yoda_attachment: 'this is'})
-          }
-        ]
-      };
       http(uploadData, function(error, response, body) {
         response.statusCode.should.equal(200);
         response.headers['content-type'].should.equal('application/json; charset=utf-8');
         var uploaded = JSON.parse(body);
         http.get(baseUrl + uploaded.path, function(err, res, bod) {
           res.statusCode.should.equal(200);
+          var actual_attachment = JSON.parse(bod);
+          actual_attachment['yoda'].should.equal(attachment['yoda']);
           done();
         });
       });
