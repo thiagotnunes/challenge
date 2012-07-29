0742d1021c70443dba50b99adfd16dad6e30fdb6
Thiago Nunes <thiagotnunes@gmail.com>
2012-07-29 01:29:35 -0300

    Adding tests for setting the temporary directory.

--- test/uploads_parser_test.js
+++ test/uploads_parser_test.js
@@ -4,9 +4,20 @@ var expect = chai.expect;
 var sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 
-var uploadsParser = require('../lib/uploads_parser')();
+var uploadDir = 'uploadDir';
+var uploadsParser = require('../lib/uploads_parser')(uploadDir);
 
 describe('Uploads parser', function() {
+
+  it('should set the uploads directory', function() {
+    var form = {
+      parse: function() {}
+    };
+
+    uploadsParser.handle(form);
+
+    form.uploadDir.should.be.equal(uploadDir);
+  });
 
   it('should parse the form', function() {
     var form = {
