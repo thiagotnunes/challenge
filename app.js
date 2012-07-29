5b51ff05d0951a27d65e17464cca04bbab76ba2d
Thiago Nunes <thiagotnunes@gmail.com>
2012-07-29 15:45:00 -0300

    Binding the uploads tracker to measure progress on the file upload.

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 var uploadPath = '/public/uploads';
 var TMPDIR = __dirname + uploadPath;
+
+// Custom modules
 var filesParser = require('./lib/files_parser')(uploadPath);
-var uploadsParser = require('./lib/uploads_parser')(TMPDIR, filesParser);
+var uploadsTracker = require('./lib/uploads_tracker')();
+var uploadsParser = require('./lib/uploads_parser')(TMPDIR, filesParser, uploadsTracker);
 
+// Vendor modules
 var formidable = require('formidable');
-var app = require('express').createServer();
+var express = require('express');
 
-app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/public/index.html');
-}); 
+var app = express();
+
+app.configure('development', function(){
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
 app.get('/public/uploads/:filename', function(req, res) {
   res.sendfile(__dirname + '/public/uploads/' + req.params.filename);
