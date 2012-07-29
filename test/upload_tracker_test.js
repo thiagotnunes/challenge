d32e5d5c1fdcf46cdaf2e6a25d19a88af9bcdc1f
Thiago Nunes <thiagotnunes@gmail.com>
2012-07-29 02:52:44 -0300

    Upload tracker for the uploads progress.

---
+++ test/upload_tracker_test.js
@@ -0,0 +1,21 @@
+describe('Upload tracker', function() {
+
+  var tracker;
+
+  beforeEach(function() {
+    tracker = require('../lib/upload_tracker.js')();
+  });
+
+  it('should retrieve the percentage of a given file', function() {
+    var file = "fileId";
+    var percentage = 25.54674;
+    tracker.setProgress(file, percentage);
+
+    tracker.progressFor(file).should.be.equal("26");
+  });
+
+  it('should return undefined for a percentage of a file that is not in progress', function() {
+    var file = "unknown file";
+    expect(tracker.progressFor(file) === undefined).to.be.ok;
+  });
+});
