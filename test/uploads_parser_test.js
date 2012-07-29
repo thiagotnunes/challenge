b8a390e0e044e4eeb18df5abf1644518758f2227
Thiago Nunes <thiagotnunes@gmail.com>
2012-07-29 01:52:04 -0300

    Refactoring upload parser tests.

--- test/uploads_parser_test.js
+++ test/uploads_parser_test.js
@@ -4,30 +4,27 @@ var expect = chai.expect;
 var sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 
-var uploadDir = 'uploadDir';
-var uploadsParser = require('../lib/uploads_parser')(uploadDir);
-
 describe('Uploads parser', function() {
+  var form = {
+    parse: function() {},
+    on: function() {}
+  };
+  var uploadDir = 'uploadDir';
+  var uploadsParser;
+
+  beforeEach(function() {
+    uploadsParser = require('../lib/uploads_parser')(uploadDir);
+  });
 
   it('should set the uploads directory', function() {
-    var form = {
-      parse: function() {},
-      on: function() {}
-    };
-
     uploadsParser.handle(form);
 
     form.uploadDir.should.be.equal(uploadDir);
   });
 
   it('should parse the form', function() {
-    var form = {
-      parse: function() {},
-      on: function() {}
-    };
     var parse = sinon.spy(form, "parse");
     var req = sinon.stub();
-    var res = sinon.stub();
 
     uploadsParser.handle(form, req);
 
@@ -35,10 +32,6 @@ describe('Uploads parser', function() {
   });
 
   it('should bind to the progress event', function() {
-    var form = {
-      parse: function() {},
-      on: function() {}
-    };
     var on = sinon.spy(form, "on");
 
     uploadsParser.handle(form);
