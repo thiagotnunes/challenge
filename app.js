c1c09ad47ce32ff1df1b8a0f277a8d135aa21728
Thiago Nunes <thiagotnunes@gmail.com>
2012-07-29 22:57:38 -0300

    Organizing the html5 js files.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var uuid = require('node-uuid');
 
 var app = express();
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public/javascript'));
+app.use(express.static(__dirname + '/public/javascript/html5'));
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
