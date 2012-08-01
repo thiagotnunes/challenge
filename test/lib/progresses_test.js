4e9f500f971d6f812f6ab08e1f2bd40945026211
Thiago Nunes <thiagotnunes@gmail.com>
2012-08-01 11:13:45 -0300

    Creating an object to handle all the progresses tracking. I am going to remove this behavior from the DAO (yes, you heard me).

---
+++ test/lib/progresses_test.js
@@ -0,0 +1,21 @@
+describe('Progresses', function() {
+  var _progresses;
+
+  beforeEach(function() {
+    _progresses = require('../../lib/progresses.js')();
+  });
+
+  it('should retrieve the percentage of a given file', function() {
+    var id = "fileId";
+    var percentage = 25.54674;
+    _progresses.setProgress(id, percentage);
+
+    expect(_progresses.progressFor(id)).to.equal("26");
+  });
+
+  it('should return undefined for a percentage of a file that is not in progress', function() {
+    var id = "unknown file";
+
+    expect(_progresses.progressFor(id) === undefined).to.be.ok;
+  });
+});
